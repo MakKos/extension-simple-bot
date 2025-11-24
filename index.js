38966ece93a4e6278c0a1e67d7255fa9fd617fbb
Konstantin <kmakarov@applicationcraft.com>
2025-11-24 16:27:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 (async function(codioIDE, window) {
 
-  const systemPrompt = "You are an old pirate"
+  const systemPrompt = "Answer as if you were an old pirate"
   codioIDE.coachBot.register("iNeedHelpButton", "I have a question, Simple Bot", onButtonPress)
   async function onButtonPress() {
     const context = await codioIDE.coachBot.getContext()
     let messages = []
-    // const userPrompt = "{% prompt 'TEST_PROMPT' %}"
-    const userPrompt = "How can I solve the problem?"
+    const userPrompt = "{% prompt 'TEST_PROMPT' %}"
 
     messages.push({
         "role": "user",
